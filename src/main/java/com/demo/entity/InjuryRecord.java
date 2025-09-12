cb9a87bcdacd5b735d987bd1820a1f2e7b5a0239
hfutwz <113979406+hfutwz@users.noreply.github.com>
2025-09-12 13:27:01 +0800

    3-时间线-不同的干预方式的时间

--- src/main/java/com/demo/entity/InjuryRecord.java
+++ src/main/java/com/demo/entity/InjuryRecord.java
@@ -86,3 +86,32 @@ public class InjuryRecord implements Serializable {
     private Double latitude;
 
 }
+/**
+ * 建表语句
+ * CREATE TABLE InterventionTime (
+ *     intervention_id INT AUTO_INCREMENT PRIMARY KEY COMMENT '干预方式ID',
+ *     patient_id INT NOT NULL COMMENT '患者ID,外键',
+ *     admission_date DATE NOT NULL COMMENT '接诊日期',
+ *     admission_time VARCHAR(4) NOT NULL COMMENT '接诊时间',
+ *     -- 各干预方式的时间点，存储为4位字符串（HHMM），为空表示无此干预
+ *     peripheral VARCHAR(4) DEFAULT NULL COMMENT '外周',
+ *     iv_line VARCHAR(4) DEFAULT NULL COMMENT '深静脉',
+ *     central_access VARCHAR(4) DEFAULT NULL COMMENT '骨通道',
+ *     nasal_pipe VARCHAR(4) DEFAULT NULL COMMENT '鼻导管',
+ *     face_mask VARCHAR(4) DEFAULT NULL COMMENT '面罩',
+ *     endotracheal_tube VARCHAR(4) DEFAULT NULL COMMENT '气管插管',
+ *     ventilator VARCHAR(4) DEFAULT NULL COMMENT '呼吸机开始时间',
+ *     cpr_start_time VARCHAR(4) DEFAULT NULL COMMENT '心肺复苏开始时间',
+ *     cpr_end_time VARCHAR(4) DEFAULT NULL COMMENT '心肺复苏结束时间',
+ *     ultrasound VARCHAR(4) DEFAULT NULL COMMENT 'B超',
+ *     CT VARCHAR(4) DEFAULT NULL COMMENT 'CT',
+ *     tourniquet VARCHAR(4) DEFAULT NULL COMMENT '止血带',
+ *     blood_draw VARCHAR(4) DEFAULT NULL COMMENT '采血',
+ *     catheter VARCHAR(4) DEFAULT NULL COMMENT '导尿',
+ *     gastric_tube VARCHAR(4) DEFAULT NULL COMMENT '胃管',
+ *     transfusion_start VARCHAR(4) DEFAULT NULL COMMENT '输血开始时间',
+ *     transfusion_end VARCHAR(4) DEFAULT NULL COMMENT '输血结束时间',
+ *     leave_surgery_time VARCHAR(4) DEFAULT NULL COMMENT '离开抢救室时间',
+ *     CONSTRAINT fk_intervention_patient FOREIGN KEY (patient_id) REFERENCES Patient(patient_id) -- 假设患者表为Patients，外键关联
+ *     )COMMENT='干预方式时间表';
+ */
---
+++ src/main/java/com/demo/mapper/InterventionTimeMapper.java
@@ -0,0 +1,16 @@
+package com.demo.mapper;
+
+import com.baomidou.mybatisplus.core.mapper.BaseMapper;
+import com.demo.entity.InterventionTime;
+import org.apache.ibatis.annotations.Mapper;
+import org.apache.ibatis.annotations.Select;
+
+import java.util.List;
+
+@Mapper
+public interface InterventionTimeMapper extends BaseMapper<InterventionTime> {
+
+    // 自定义查询：根据患者ID查询干预时间
+    @Select("SELECT * FROM interventiontime WHERE patient_id = #{patientId}")
+    List<InterventionTime> selectByPatientId(Integer patientId);
+}
